383f448a1bd56c9964a1a14836448284cbbb7f00
camilo <camilo.manrique@iths.se>
2024-12-12 22:04:04 +0100

    klaaaaaar

--- src/main/java/com/example/tictactoe6/HelloApplication.java
+++ src/main/java/com/example/tictactoe6/HelloApplication.java
@@ -11,7 +11,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/example/tictactoe6/hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 300, 650);
+        Scene scene = new Scene(fxmlLoader.load(), 300, 651);
         stage.setTitle("Tic Tac Toe");
         stage.setScene(scene);
         stage.show();
